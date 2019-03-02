c10dbfa7366ed46d8814d06120bfaaa943da4977
anniemoss <moss94549@gmail.com>
2019-03-01 18:59:22 -0800

    Phase 2: Auto generate html files from content files and base template

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import os
 import glob
 import os.path
+from jinja2 import Template
 
 DEBUG=True
-
 
 #
 # Remove all *.html file from directory
@@ -16,7 +16,6 @@ DEBUG=True
 def cleanDir(directory):
     if DEBUG:
         print("DEBUG: utils.py.cleanDir()")
- 
     # Get a list of all the file paths that end with .html from the directory
     fileList = glob.glob(directory + "/*.html")
     if DEBUG:
@@ -32,7 +31,7 @@ def cleanDir(directory):
     for filePath in fileList:
         try:
             os.remove(filePath)
-            if DEBUG:         
+            if DEBUG:            
                 print("DEBUG: utils.py.cleanDir().return(TRUE)")                
             return True
         except:
@@ -40,37 +39,6 @@ def cleanDir(directory):
             if DEBUG:            
                 print("DEBUG: utils.py.cleanDir().return(FALSE)")                            
             return False
-
-
-#
-#   Create a list containing data to be inserted into a web page via Templating
-#
-#   prefixes: a list of unique words
-#   filesnames: a dictionary where:
-#           key: can be found in 'prefixes'
-#           values: a list of file names which contain content common to a single web page
-#   outputs: the name of the output html file
-#   titles: title of a web page. 
-#  
-#
-def getTemplateData(prefixes, filenames, outputs, titles):
-    if DEBUG:
-        print("DEBUG: utils.py.getTemplateData()")
-    pages=[]
-
-    if DEBUG:
-        print("DEBUG: utils.py.getTemplateData().filenames=", type(filenames))
-        print("DEBUG: utils.py.getTemplateData().outputs", type(outputs))
-        print("DEBUG: utils.py.getTemplateData().titles", type(titles))
-    
-    for prefix in prefixes:
-        if DEBUG:
-            print("DEBUG: utils.py.getTemplateData().prefix=", prefix)
-            print("DEBUG: utils.py.getTemplateData().filenames[",prefix, "]", filenames[prefix])
-            print("DEBUG: utils.py.getTemplateData().outputs[", prefix,"]=", outputs[prefix])
-            print("DEBUG: utils.py.getTemplateData().titles[", prefix,"]=", titles[prefix])                       
-        pages.append({"filenames":filenames[prefix],  "output":outputs[prefix], "title":titles[prefix]})
-    return pages
 
 
 #
@@ -118,53 +86,6 @@ def getPrefixes(strings):
 
 
 #
-# Build a list of files grouped based on common prefix in their filenames
-# which compose the site content
-#
-# Input:
-#   files : list containg files which wil be orgainzed in hash/dictionary.
-#            Every file is expected to have the following format:
-#                <content_dir> + "/<prefix>_<tag>.html"
-#            Where:
-#               'prefix' is the name of the page for which content comprises and determines the grouping of files
-#                      
-# Returns:
-#   A dictionary of lists containing file names.
-#   Each dictionry key is a 'prefix' common to all the corresponding file names
-#   Format 
-#    "prefix" : [ <content_dir> + "/<prefix>_<tag>.html", ...]
-#        
-def getFilenames(files, prefixes):
-    if DEBUG:
-        print("DEBUG: utils.py.getFilenames()")
-    # Local varibles
-    filenames={}  # files grouped by page
-
-    # Create list of page content by page
-    # (group files with the same prefix)
-    for filename in files:
-        if DEBUG:        
-            print("DEBUG: utils.py.getFilenames().file_name=", filename)
-        for prefix in prefixes:
-            if DEBUG:
-                print("DEBUG: utils.py.getFilenames().prefix=", prefix)
-            if prefix in filename:
-                if DEBUG:
-                    print("DEBUG: utils.py.getFilenames().prefix = filename")
-                # Add file to page content grouping
-                if prefix in filenames.keys():
-                    if DEBUG:                    
-                        print("DEBUG: utils.py.getFilenames().if: adding element to list")
-                    filenames[prefix].append(filename)
-                else:
-                    # Create new page content grouping with first value                
-                    filenames[prefix]=[filename]
-                if DEBUG:
-                    print("DEBUG: utils.py.getFilenames().grouped files=", filenames)
-    return filenames
-
-
-#
 # Generate files names with '.html' suffix
 #
 # Input:
@@ -192,16 +113,13 @@ def getPath(odir,prefixes):
         print("DEBUG: utils.py.getPath().outputs:", outputs)    
     return outputs
         
-        
- #
- # Capitalize the first letter of a word and
- # convert remainder to lower case
- #  Inpur:
- #      a string
- #
- #  Output:
- #      lowercase string wit first letter uppercase
- #
+#
+#   Convert a string to a title (first letter capital the rest lower case)
+#   Input:
+#       string to be converted
+#
+#   
+#
 def getTitles(prefixes):
     if DEBUG:   
         print("DEBUG: utils.py.getTitles()")   
@@ -217,111 +135,56 @@ def getTitles(prefixes):
         
     return titles
 
-
-#       
-# Construct a web page using templating technique
+#
+# Render a web page using the Templating technique
 #
 # Input:
-#   base_dir : the directory containg templating base files
-#   page : a dictionary representation of page content
-#       Where:
-#         'page' format:
-#            {
-#                "filenames" : [ "full_path_of_content_file1.html", ...]  -> content used in templating
-#                "title"     :  "web_page_title",
-#            }
-#           where:
-#               "full_path_of_content_file1.html":
-#                          Every file is expected to have the following format: <prefix>_'<tag>'.html.
-#                          Where
-#                               'prefix': the name of the page for which the file content comprises
-#                               'tag'   : is either:
-#                                   'msg'  -> this content will replace content in a base file with template tag {{content_msg}}
-#                                   'main' -> this content will replace content in a base file with template tag {{content_main}}
+#   cdir: directory containing template content used to compase page
+#   odir: generated web pages output directory
+#   basefile: template file
 #
-# Return : text representation of a page
-#
-# TODO:
-# 1. make 'msg' and 'main' parameters so for loop can become a function
-#    
-def getPage(template, page):
-    if DEBUG:
-        print("DEBUG: utils.py.getPage()")
-    
-    # Open the templae
-    pcontent=open(template, 'r').read()
-
-    # Determmine if any dynamic content exists for page
-    if "filenames" in page.keys(): 
-        # Determine if there are msg files
-        if not any("msg" in s for s in page["filenames"]):
-            pcontent=pcontent.replace("{{content_msg}}","") 
-        # Determine if there are main files    
-        if not any("main" in s for s in page["filenames"]):
-            pcontent=pcontent.replace("{{content_main}}","")
-            
-        # Add title to page
-        pcontent=pcontent.replace("{{title}}","")
-            
-        # Replace base templates with dynamic content
-        for mfile in page["filenames"]:
+def render(cdir,odir, basefile, prefixes):  
+    for prefix in prefixes:
+        # open/ reposition file pointer
+        template_html = open(basefile).read()
+        # initialize template
+        template = Template(template_html)
+        
+        # Get content if no content file exists set variable to " "
+        try:
             if DEBUG:
-                print("DEBUG: utils.py.getPage().mfile=", mfile)
-            if "main" in mfile:
-                if DEBUG:                
-                    print("DEBUG: utils.py.getPage().if: if main in ", mfile)                                    
-                with open(mfile, 'r') as fcontent:
-                    pcontent=pcontent.replace("{{content_main}}", fcontent.read())
-            elif "msg" in mfile:
-                if DEBUG:                
-                    print("DEBUG: utils.py.getPage().elif: msg in ", mfile)
-                with open( mfile, 'r') as fcontent:
-                    pcontent=pcontent.replace("{{content_msg}}", fcontent.read())
-    else: # No Dynamic content
+                print("DEBUG: utils.py.render().open(", cdir + prefix + "_main.html).read()")             
+            main_html = open(cdir + prefix + "_main.html").read()               
+        except:
+            main_html = " "
+
+        try:
+            if DEBUG:
+                print("DEBUG: utils.py.render().open(", cdir + prefix + "_msg.html.read()")                            
+            msg_html  = open(cdir + prefix + "_msg.html").read()
+        except:
+            msg_html = " "
+
+        titles = getTitles(prefixes)
+        links = getPath(odir, prefixes)
         if DEBUG:
-            print("DEBUG: utils.py.getPage().else: no Dynamic Content")
-        # Create page with no additional content       
-        pcontent=pcontent.replace("{{content_msg}}","")
-        pcontent=pcontent.replace("{{content_main}}","")
+            print("DEBUG: utils.py.render().links:", links)        
+            print("DEBUG: utils.py.main_html=", main_html)
+            print("DEBUG: utils.py.msg_html=", msg_html)
+        result = template.render(
+#            title=titles[prefix],
+            title=prefix,
+            content_main=main_html,
+            content_msg=msg_html,
+            links=links,           
+        )
         
-    if DEBUG:
-        print("DEBUG: utils.py.getPage().return:", pcontent)    
-    return pcontent
-
-
-#
-# Create a dictionary representation of page content
-# The dictionary format will be:
-#{
-#    "filenames" : [ content_dir + "/projects_main.html",],
-#    "output"    :  output_dir + "/projects.html",
-#    "title"     :  "Projects",
-#},    
-def getPages(content_dir,output_dir):
-    filenames = {}
-    outputs   = {}
-    titles    = {}
-    
-    # Get Content Files
-    content_files=getContentFiles(content_dir)
-    # Get list of unique filenam prefixes => filename upto '_'
-    prefixes=getPrefixes(content_files)
-    
-    # Get output ile names
-    outputs=getPath(output_dir, prefixes)
-    if DEBUG:
-        print("DEBUG: utils.py.getPages().outputs=", outputs)
-        
-    filenames=getFilenames(content_files, prefixes)
-    if DEBUG:
-        print("DEBUG: utils.py.getPages().filenames=", filenames)    
-
-    titles=getTitles(prefixes)
-    if DEBUG:    
-        print("DEBUG: utils.py.getPages().titles=", titles)
-    
-    # Create the pages dictionary
-    pages=getTemplateData(prefixes, filenames, outputs, titles)
-    if DEBUG:    
-        print("DEBUG: utils.py.getPages().pages=", pages)
-    return pages
+        # Write out template
+        if prefix == 'home':
+            if DEBUG:
+                print("DEBUG: utils.py.render().else: print to->", odir + 'index.html' )                                
+            open(odir + 'index.html', 'w+').write(result)
+        else:
+            if DEBUG:
+                print("DEBUG: utils.py.render().else: print to->", odir + prefix + '.html' )                    
+            open(odir + prefix + '.html', 'w+').write(result)        
